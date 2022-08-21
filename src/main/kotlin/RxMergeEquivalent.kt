d4cb85652a48dd167cd702262e81c850c7458f65
Keun Young Park <purken8857@gmail.com>
2022-08-21 23:10:45 +0900

    Code Cleanup

--- src/main/kotlin/RxMergeEquivalent.kt
+++ src/main/kotlin/RxMergeEquivalent.kt
@@ -1,41 +1,44 @@
 import kotlinx.coroutines.*
 
 class RxMergeEquivalent {
-    val scope = CoroutineScope(Dispatchers.IO + CoroutineName("AfterMerge"))
+    private val scope = CoroutineScope(Dispatchers.IO + CoroutineName("AfterMerge"))
 
     fun mergeTwoAsyncsAndRunAfterTheyAreAllDoneUsingExplicitJoin() {
         runBlocking {
-            println("startBlocking")
             val job1 = scope.launch {
-                delay(2000)
-                println("Runs first")
+                runFirst()
             }
             val job2 = scope.launch {
-                delay(3000)
-                println("Runs second")
+                runSecond()
             }
             val list = listOf(job1, job2)
             list.joinAll()
 
             println("running after job1 and job2 are done. Job1 and Job2 are launched in parallel")
-            println("doneBlocking")
         }
     }
 
     fun mergeTwoAsyncsAndRunAfterTheyAreAllDoneUsingStructuredConcurrency() {
         runBlocking {
-            println("startBlocking")
-            launch {
-                delay(2000)
-                println("Runs first")
+            launch(Dispatchers.IO) {
+                runFirst()
             }
-            launch {
+            launch(Dispatchers.IO) {
                 delay(3000)
                 println("Runs Second")
             }
-            println("doneBlocking")
         }
 
         println("running after job1 and job2 are done. Job1 and Job2 are launched in parallel")
+    }
+
+    private suspend fun runFirst() {
+        delay(2000)
+        println("Runs first")
+    }
+
+    private suspend fun runSecond() {
+        delay(3000)
+        println("Runs second")
     }
 }
