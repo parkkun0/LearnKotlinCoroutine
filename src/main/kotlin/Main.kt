b5254b9a86f0a02635ded33ed1c76929d5c50d88
Keun Young Park <purken8857@gmail.com>
2022-08-21 22:58:42 +0900

    Test result

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,5 +9,17 @@ fun main() {
     RxMergeEquivalent().mergeTwoAsyncsAndRunAfterTheyAreAllDoneUsingStructuredConcurrency()
     println("==========================================")
 
+    println("""
+        Discovered that Join and Structured Concurrency are different in the way they handle after second coroutine launch.
+        
+        Using join, println after second job was blocked until both jobs are done.
+        
+        On the other hand, using structured concurrency, println following the second job was immediately run.
+        The parent coroutine was just not done because all its children weren't done.
+        
+        Thus, to use structured concurrency another explicit coroutine launch is necessary to execute what is intended 
+        
+    """.trimIndent())
+
     println("program ends")
 }
