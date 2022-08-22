d5f21034ed332cd5789162b7f90a78ed22d5fcc7
Keun Young Park <purken8857@gmail.com>
2022-08-22 22:50:38 +0900

    Learn about async behavior on repeated await call to the same Deferred

--- src/main/kotlin/MultipleAsyncLearning.kt
+++ src/main/kotlin/MultipleAsyncLearning.kt
@@ -10,30 +10,55 @@ fun main() {
         But it turns out that the code ran deferred 1 and two in parallel and waited for both to complete.
         ==========================
     """.trimIndent())
+
+
+    MultipleAsyncLearning().checkIfDeferred1SecondAwaitCallsAsyncAgain()
+    println("""
+        ========Result============
+        Observed that any awaits following after first completion does not trigger async again.
+        Seems like it is using cached(completed) value
+        ==========================
+    """.trimIndent())
 }
 
 class MultipleAsyncLearning {
     fun checkIfAwaitIsRunOneAfterOneAnother() {
         runBlocking {
-            val deferred1 = async {
-                println("Deferred1 starting")
-                delay(5000)
-                println("Deferred1 ending")
-                return@async "first"
-            }
-            val deferred2 = async {
-                println("Deferred2 starting")
-                delay(3000)
-                println("Deferred2 ending")
-                return@async 1234
-            }
+            val deferred1 = getAsync1()
+            val deferred2 = getAsync2()
             println("Is this piece of code run before or after two async declaration?")
             showResultForDeferredAwait(deferred1.await(), deferred2.await())
             println("Is this piece of code run before or after both deferred are complete?")
         }
     }
 
+    private fun CoroutineScope.getAsync2() = async {
+        println("Deferred2 starting")
+        delay(3000)
+        println("Deferred2 ending")
+        return@async 1234
+    }
+
+    private fun CoroutineScope.getAsync1() = async {
+        println("Deferred1 starting")
+        delay(5000)
+        println("Deferred1 ending")
+        return@async "first"
+    }
+
     private fun showResultForDeferredAwait(result1: String, result2: Int) {
         println("1: $result1, 2:$result2")
+    }
+
+    fun checkIfDeferred1SecondAwaitCallsAsyncAgain() {
+        runBlocking {
+            val deferred1 = getAsync1()
+            val deferred2 = getAsync2()
+            println("Is this piece of code run before or after two async declaration?")
+            showResultForDeferredAwait(deferred1.await(), deferred2.await())
+            println("Does await on already completed async trigger async again?")
+            println("Deferred1: ${deferred1.await()}")
+            println("Is this piece of code run before or after both deferred are complete?")
+        }
     }
 }
